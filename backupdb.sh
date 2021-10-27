9cd7dde4bb131921faa9b938f3003faa8095f9f4
Gilang Pratama <pratamapriadi96@gmail.com>
2021-10-28 03:25:19 +0700

    :zap: update script

--- backupdb.sh
+++ backupdb.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
 export PATH=/bin:/usr/bin:/usr/local/bin
-TODAY=`date +"%d%m%Y%H%I%S"`
+TODAY=$(date +"%d%m%Y%H%I%S")
 
 ################################################################
 ################## Update below values  ########################
-DB_BACKUP_PATH='/home/akasakaryu/backups/siang'
+DB_BACKUP_PATH='/home/akasakaryu/backups/pagi'
 MYSQL_HOST='localhost'
 MYSQL_PORT='3306'
 MYSQL_USER='fath_dev'
 MYSQL_PASSWORD='fathtech123'
-BACKUP_RETAIN_DAYS=3
+BACKUP_RETAIN_DAYS=1
 #################################################################
 
 # get a list of databases
-databases=`mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} \
--e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+databases=$(mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} \
+    -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
 # Create folder
 mkdir ${DB_BACKUP_PATH}/${TODAY}
 
 # backup all databases
 for DATABASE_NAME in $databases; do
-    if [ $DATABASE_NAME != 'mysql' ] && [ $DATABASE_NAME != 'phpmyadmin' ] && [ $DATABASE_NAME != 'information_schema' ] && [ $DATABASE_NAME != 'performance_schema' ] && [ $DATABASE_NAME != 'test' ]
-    then
+    if [ $DATABASE_NAME != 'mysql' ] && [ $DATABASE_NAME != 'phpmyadmin' ] && [ $DATABASE_NAME != 'information_schema' ] && [ $DATABASE_NAME != 'performance_schema' ] && [ $DATABASE_NAME != 'test' ]; then
         echo "Backup started for database - ${DATABASE_NAME}"
 
         mysqldump -h ${MYSQL_HOST} \
-        -P ${MYSQL_PORT} \
-        -u ${MYSQL_USER} \
-        -p${MYSQL_PASSWORD} \
-        ${DATABASE_NAME} > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}@${MYSQL_HOST}_${TODAY}.sql
+            -P ${MYSQL_PORT} \
+            -u ${MYSQL_USER} \
+            -p${MYSQL_PASSWORD} \
+            ${DATABASE_NAME} >${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}@${MYSQL_HOST}_${TODAY}.sql
 
         if [ $? -eq 0 ]; then
             echo "Database backup successfully completed"
@@ -42,7 +41,7 @@ for DATABASE_NAME in $databases; do
 done
 
 # compress backup folder
-zip -r ${DB_BACKUP_PATH}/${TODAY}.zip ${DB_BACKUP_PATH}/${TODAY}/
+cd ${DB_BACKUP_PATH} && zip -r ${TODAY}.zip ${TODAY}
 
 # check compressing status
 if [ $? -eq 0 ]; then
@@ -56,4 +55,4 @@ fi
 find ${DB_BACKUP_PATH} -name "*.zip" -type f -mtime +${BACKUP_RETAIN_DAYS} -exec rm -f {} \;
 
 echo -------------------------------------
-echo   Finished!
+echo Finished!
--- inquiryv2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-    php /home/akasakaryu/situs/sisfomav2/html/index.php tools virtacc inquiry
-    sleep 5
-done
